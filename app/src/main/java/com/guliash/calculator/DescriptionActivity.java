f4d3c82a2662ae48a774ba4cac87524ffb8680d8
Artem Gilmudinov <guliash92@gmail.com>
2016-05-19 22:30:32 +0300

    faq topics;

---
+++ app/src/main/java/com/guliash/calculator/DescriptionActivity.java
@@ -0,0 +1,37 @@
+package com.guliash.calculator;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class DescriptionActivity extends AppCompatActivity {
+
+    private TextView mDescText, mNameText;
+    private ListView mExamplesList;
+
+    private Topic mTopic;
+
+    public static final String TOPIC = "topic";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desc);
+
+        Bundle args = (savedInstanceState != null ? savedInstanceState : getIntent().getExtras());
+
+        mTopic = args.getParcelable(TOPIC);
+
+        mNameText = (TextView)findViewById(R.id.name);
+        mDescText = (TextView)findViewById(R.id.desc);
+        mExamplesList = (ListView)findViewById(R.id.examples);
+
+        mNameText.setText(mTopic.name);
+        mDescText.setText(mTopic.description);
+        mExamplesList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                mTopic.examples));
+    }
+}
