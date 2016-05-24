9cd64539414c04f5c837cb478288b174c4a8132c
Artem Gilmudinov <guliash92@gmail.com>
2016-05-24 22:17:01 +0300

    checking menu opens activities ok;

--- app/src/androidTest/java/com/github/guliash/calculator/MainActivityTest.java
+++ app/src/androidTest/java/com/github/guliash/calculator/MainActivityTest.java
@@ -1,13 +1,19 @@
 package com.github.guliash.calculator;
 
-import android.support.test.espresso.Espresso;
+import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.intent.matcher.IntentMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
 import android.widget.TextView;
 
 import com.guliash.calculator.R;
+import com.guliash.calculator.ui.activities.HelpActivity;
 import com.guliash.calculator.ui.activities.MainActivity;
+import com.guliash.calculator.ui.activities.OpenActivity;
+import com.guliash.calculator.ui.activities.SaveActivity;
+import com.guliash.calculator.ui.activities.SettingsActivity;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -16,11 +22,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.AnyOf.anyOf;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -34,21 +44,86 @@ public class MainActivityTest {
     @Test
     public void checkBackspaceButtonRemoves() {
         String stringToTypeIn = "123";
-        Espresso.onView(withId(R.id.input_field)).perform(typeText(stringToTypeIn));
-        Espresso.onView(withId(R.id.backspace)).perform(click());
-        Espresso.onView(withId(R.id.input_field)).check(matches(withText(stringToTypeIn.substring(0,
+        onView(withId(R.id.input_field)).perform(typeText(stringToTypeIn));
+        onView(withId(R.id.backspace)).perform(click());
+        onView(withId(R.id.input_field)).check(matches(withText(stringToTypeIn.substring(0,
                 stringToTypeIn.length() - 1))));
     }
 
     @Test
     public void checkEqualsCalculates() {
         String stringToTypeIn = "2 + 3";
-        Espresso.onView(withId(R.id.input_field)).perform(typeText(stringToTypeIn));
-        Espresso.onView(withId(R.id.equals_image_button)).perform(click());
-        Espresso.onView(withId(R.id.result_field)).check(matches(checkCalculatedValue(5.0)));
+        onView(withId(R.id.input_field)).perform(typeText(stringToTypeIn));
+        onView(withId(R.id.equals_image_button)).perform(click());
+        onView(withId(R.id.result_field)).check(matches(withDouble(5.0)));
     }
 
-    public static Matcher<View> checkCalculatedValue(final double expected) {
+    @Test
+    public void checkOpenMenuItemStartsOpenActivity() {
+        Intents.init();
+
+        try {
+            onView(anyOf(withId(R.id.open), withText(R.string.open))).perform(click());
+        } catch (NoMatchingViewException e) {
+            openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+            onView(withText(R.string.open)).perform(click());
+        }
+
+        Intents.intended(IntentMatchers.hasComponent(OpenActivity.class.getName()));
+
+        Intents.release();
+    }
+
+    @Test
+    public void checkHelpMenuItemStartsHelpActivity() {
+        Intents.init();
+
+        try {
+            onView(anyOf(withId(R.id.help), withText(R.string.help))).perform(click());
+        } catch (NoMatchingViewException e) {
+            openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+            onView(withText(R.string.help)).perform(click());
+        }
+
+        Intents.intended(IntentMatchers.hasComponent(HelpActivity.class.getName()));
+
+        Intents.release();
+    }
+
+    @Test
+    public void checkSaveMenuItemStartsSaveActivity() {
+        Intents.init();
+
+        try {
+            onView(anyOf(withId(R.id.save), withText(R.string.save))).perform(click());
+        } catch (NoMatchingViewException e) {
+            openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+            onView(withText(R.string.save)).perform(click());
+        }
+
+        Intents.intended(IntentMatchers.hasComponent(SaveActivity.class.getName()));
+
+        Intents.release();
+    }
+
+    @Test
+    public void checkSettingsMenuItemStartsSettingsActivity() {
+        Intents.init();
+
+        try {
+            onView(anyOf(withId(R.id.settings), withText(R.string.settings))).perform(click());
+        } catch (NoMatchingViewException e) {
+            openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+            onView(withText(R.string.settings)).perform(click());
+        }
+
+        Intents.intended(IntentMatchers.hasComponent(SettingsActivity.class.getName()));
+
+        Intents.release();
+    }
+
+
+    public static Matcher<View> withDouble(final double expected) {
         return new TypeSafeMatcher<View>() {
             @Override
             protected boolean matchesSafely(View item) {
@@ -67,5 +142,7 @@ public class MainActivityTest {
             }
         };
     }
+
+
 
 }
