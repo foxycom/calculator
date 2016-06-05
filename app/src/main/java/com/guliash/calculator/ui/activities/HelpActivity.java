02358f7e43689edaeb7626023e05e17a7daafd47
guliash <guliash92@gmail.com>
2016-06-05 17:29:26 +0300

    collapsed empty tags; removed help menu;

--- app/src/main/java/com/guliash/calculator/ui/activities/HelpActivity.java
+++ app/src/main/java/com/guliash/calculator/ui/activities/HelpActivity.java
@@ -72,13 +72,6 @@ public class HelpActivity extends BaseActivity {
         return topics;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_help, menu);
-        return true;
-    }
-
     private void itemClicked(Topic topic) {
         Intent intent = new Intent(this, DescriptionActivity.class);
         intent.putExtra(DescriptionActivity.TOPIC, topic);
